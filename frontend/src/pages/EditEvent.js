bc3d5c3a496bce4a9a5daf63f9ecb73a17746467
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-07-04 13:58:26 +0700

    update data

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,5 +1,5 @@
-import { useRouteLoaderData } from "react-router-dom";
-import EventForm from "./../components/EventForm";
+import { json, useRouteLoaderData } from 'react-router-dom';
+import EventForm from './../components/EventForm';
 
 function EditEventPage() {
   const data = useRouteLoaderData('event-detail');
@@ -8,3 +8,32 @@ function EditEventPage() {
 }
 
 export default EditEventPage;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  const id = params.eventId;
+
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  const response = await fetch('http://localhost:8081/events/' + id, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: 'Could not update a event.' },
+      {
+        status: 500,
+      }
+    );
+  }
+}
