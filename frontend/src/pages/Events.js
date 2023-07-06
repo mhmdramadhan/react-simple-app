b0f623091102363ec7583256ef3b2294377a97ff
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-07-06 07:03:59 +0700

    await data events when get data from api

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,20 +1,23 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const fetchData = useLoaderData();
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={fetchData} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8081/events");
 
   if (!response.ok) {
@@ -29,4 +32,10 @@ export async function loader() {
     const resData = await response.json();
     return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
